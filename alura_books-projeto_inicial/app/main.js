3236ea09bd849ef88c4fa58715025aa370f23d2b
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-05-03 15:10:52 -0300

    Construção do filtro

--- alura_books-projeto_inicial/app/main.js
+++ alura_books-projeto_inicial/app/main.js
@@ -6,27 +6,15 @@ getBuscarLivrosDaAPI ()
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json()
-    console.table(livros)
-    exibirOsLivros(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivros(livrosComDesconto)
 }
 
 const elementoParaInserirLivros = document.querySelector('#livros')
 
-function exibirOsLivros (listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-        `
-    });
-}
+
+
 
 //Fazer exercicios de For e Foreach
+
+//Fazer exercícios para cada método aprendido no curso
