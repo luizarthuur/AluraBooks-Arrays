d91f46c14097122ccf0855d24a1e193be3326216
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-05-02 22:51:58 -0300

    Update para botar os livros na tela

--- alura_books-projeto_inicial/app/main.js
+++ alura_books-projeto_inicial/app/main.js
@@ -7,5 +7,26 @@ async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json()
     console.table(livros)
+    exibirOsLivros(livros)
 }
 
+const elementoParaInserirLivros = document.querySelector('#livros')
+
+function exibirOsLivros (listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+        `
+    });
+}
+
+//Fazer exercicios de For e Foreach
