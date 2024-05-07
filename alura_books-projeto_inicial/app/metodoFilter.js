82f9e62e3143a5b00632da1f01bd733dd82f19c3
Luiz Arthur <LUIZARTHURWINTER@GMAIL.COM>
2024-05-07 08:14:53 -0300

    atualização do metodo filter

--- alura_books-projeto_inicial/app/metodoFilter.js
+++ alura_books-projeto_inicial/app/metodoFilter.js
@@ -1,10 +1,19 @@
 const botoes = document.querySelectorAll('.btn');
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+const btnLivrosDisponiveis  = document.querySelector('#btnLivrosDisponiveis');
+
+btnLivrosDisponiveis.addEventListener('click', () => {
+    const livrosDisponiveis = livros.filter(livro => {
+        return livro.quantidade > 0
+    })
+    exibirOsLivros(livrosDisponiveis);
+})
+
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => 
+    livros.filter(livro => 
         livro.categoria == categoria)
     exibirOsLivros(livrosFiltrados)
 }
